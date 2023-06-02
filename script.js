8769219d75895406ebcf606031b9f14fdd5950f8
seu nome <gustavo9.999soares@hotmail.com>
2023-06-02 20:01:13 -0300

    primeiro

---
+++ script.js
@@ -0,0 +1,10 @@
+let dd = document.getElementById("dias");
+let hh = document.getElementById("horas");
+let mm = document.getElementById("minutos");
+let ss = document.getElementById("segundos");
+
+let ponto_dias = document.querySelector(".ponto_dia");
+let ponto_horas = document.querySelector(".ponto_horas");
+let ponto_minutos = document.querySelector(".ponto_minutos");
+let ponto_segundos = document.querySelector(".ponto_segundos");
+
